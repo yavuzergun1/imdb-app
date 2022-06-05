5996c75cd67f26dcddd557e9264a0a78652b3729
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-06-05 15:16:46 +0300

    commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import useAxios from 'axios-hooks';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     const [{ data, loading, error }, refetch] = useAxios(
+    'https://reqres.in/api/users?delay=1'
+  )
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+
+  return (
+    <div>
+      <button onClick={refetch}>refetch</button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  )
     </div>
   );
 }
