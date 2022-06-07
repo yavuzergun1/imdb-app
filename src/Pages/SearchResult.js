11b4f076308190a5d0962a4ffd88e259955a169a
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-06-07 15:28:31 +0300

    commited

--- src/Pages/SearchResult.js
+++ src/Pages/SearchResult.js
@@ -5,26 +5,25 @@ import Search from "../Components/Search";
 import { UseData } from "../Context/Context";
 
 function SearchResult() {
-  const { data} = UseData();
-  const addFavorites= ()=>{
-  }
+  const { data } = UseData();
+  const addFavorites = () => {};
   if (!data.Search) {
     return <p>loading</p>;
   }
   return (
     <div>
       SearchResulta geldinnnn
-      {data.Search.map((film, index)=>{
-        return(
-          <div key={`/${index}`}> 
-<Link  to={`/${index}`}><img src={film.Poster} alt="" /> </Link>
-{/* <input type="checkbox"  onClick={()=>{return setFavorites([...favorites, film.imdbID]) } } /> */}
-<AddFavorites film={film} />
-</div> )
-      })
-      }
+      {data.Search.map((film, index) => {
+        return (
+          <div key={`/${index}`}>
+            <Link to={`/${index}`}>
+              <img src={film.Poster} alt="" />{" "}
+            </Link>
+            <AddFavorites film={film} />
+          </div>
+        );
+      })}
       <Search />
-      
     </div>
   );
 }
