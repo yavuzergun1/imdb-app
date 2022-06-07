d0bc3c782fce2bef78ddc62cc029b09645f984b0
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-06-07 23:36:31 +0300

    commited

--- src/Pages/ResultDetail.js
+++ src/Pages/ResultDetail.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { UseData } from "../Context/Context";
 import { useEffect } from "react";
 import AddFavorites from "../Components/RDaddFavorites";
 
 function ResultDetail() {
-  const { data, filmId, setFilmId, filmDetail} =
-    UseData();
+  const { data, filmId, setFilmId, filmDetail} =UseData();
   const { id } = useParams();
-
+  const [localFilmDetail, setLocalFilmDetail]=useState();
+console.log(filmDetail);
   useEffect(() => {
     setFilmId(data.Search[id].imdbID);
-  }, []);
+  }, [data]);
 
-  if (!filmDetail) {
+  useEffect(() => {
+    localStorage.setItem("filmDetail", JSON.stringify(filmDetail));
+    setLocalFilmDetail(JSON.parse (localStorage.getItem("filmDetail")));
+    console.log(localFilmDetail);
+  }, [filmDetail]);
+  if (!localFilmDetail) {
     return <p>loading</p>;
   }
   return (
     <div>
       <Link to="/favorites">Favorites</Link>
-      {filmId}
-      <img src={filmDetail.Poster} alt="" />
+      {/* {filmId} */}
+      <img src={localFilmDetail.Poster} alt="" />
       <AddFavorites />
-      <p>{filmDetail.Plot} </p>
+      <p>{localFilmDetail.Plot} </p>
     </div>
   );
 }
--- src/Pages/SearchResult.js
+++ src/Pages/SearchResult.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AddFavorites from "../Components/SRaddFavorites";
 import Search from "../Components/Search";
@@ -6,20 +6,27 @@ import { UseData } from "../Context/Context";
 
 function SearchResult() {
   const { data } = UseData();
-  if (!data.Search) {
+  const [localData, setLocalData] = useState();
+
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(data.Search));
+    setLocalData(JSON.parse(localStorage.getItem("data")));
+  }, [])
+  console.log(localData);
+  if (!localData) {
     return <p>loading</p>;
   }
   return (
     <div>
       <Link to="/favorites">Favorites</Link>
       SearchResulta geldinnnn
-      {data.Search.map((film, index) => {
+      {localData.map((film, index) => {
         return (
           <div key={`/${index}`}>
             <Link to={`/${index}`}>
               <img src={film.Poster} alt="" />{" "}
             </Link>
-            {data.Search[index].imdbID}
+            {localData[index].imdbID}
             <AddFavorites film={film} />
           </div>
         );
