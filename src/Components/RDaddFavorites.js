4dd62ca5238cf11d0039ef03e3c69edfd04a5593
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-06-07 17:03:19 +0300

    commited

--- src/Components/RDaddFavorites.js
+++ src/Components/RDaddFavorites.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { UseData } from '../Context/Context';
+import { useState } from 'react';
 
 function RDaddFavorites() {
+    const { favorites, setFavorites, filmId } = UseData();
+    const [checked, setChecked] = useState(true);
+   console.log(favorites);
+    const addFavorite = () => {
+        setChecked(!checked);
+        if (favorites.includes(filmId) !== true) {
+          if (checked === true) {
+            setFavorites([...favorites, filmId]);
+          }
+        }
+      };
+    
+      const deleteFavorite = () => {
+        setChecked(!checked);
+        setFavorites(favorites.filter((item) => item !== filmId))
+      }
   return (
-    <div>RDaddFavorites</div>
+    <div>
+        <input
+        type="checkbox"
+        onChange={checked ? addFavorite : deleteFavorite}
+      />
+    </div>
   )
 }
 
