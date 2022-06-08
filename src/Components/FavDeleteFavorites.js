daffa65e8399fb4b17aaed25ab97dfd7ccfe3eb6
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-06-08 16:58:37 +0300

    commited

--- src/Components/FavDeleteFavorites.js
+++ src/Components/FavDeleteFavorites.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import { useState } from 'react';
-import { UseData } from '../Context/Context';
+import React from "react";
+import { useState } from "react";
+import { UseData } from "../Context/Context";
 
-function FavDeleteFavorites({favorite} ) {
+function FavDeleteFavorites({ favorite }) {
   const { favorites, setFavorites } = UseData();
   const [checked, setChecked] = useState(true);
 
   const deleteFavorite = () => {
-    // setChecked(!checked);
     setFavorites(favorites.filter((item) => item.imdbID !== favorite.imdbID));
   };
   return (
     <div>
-      <input
-        type="checkbox"
-        onChange={deleteFavorite}
-        checked={checked}
-      />
+      <input type="checkbox" onChange={deleteFavorite} checked={checked} />
     </div>
-  )
+  );
 }
 
-export default FavDeleteFavorites
+export default FavDeleteFavorites;
