f2ef1ad58f500705dc14825aca64b2a1dcf8f36c
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-06-09 10:37:21 +0300

    commited

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,27 +1,74 @@
+// import React, { useState } from "react";
+// import { Slide } from "react-slideshow-image";
+// import '../App.css'
+
+// const MultipleSlidesExample = () => {
+//   const style = {
+//     textAlign: 'center',
+//     background: 'teal',
+//     fontSize: '30px',
+//     height: '18px',
+//     width:'155px',
+//   };
+
+//   const properties = {
+//     duration: 1000,
+//     slidesToShow: 3,
+//     slidesToScroll: 2,
+//     autoplay: true,
+//     indicators: true,
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <Slide {...properties}>
+//           <div style={style}>First Slidaaaaaaaaaaaaaaaaaaaaaaae</div>
+//           <div style={style}>Second Slide</div>
+//           <div style={style}>Third Slide</div>
+//           <div style={style}>Fourth Slide</div>
+//           <div style={style}>Fifth Slide</div>
+//           <div style={style}>sixth Slide</div>
+//           <div style={style}>Seventh Slide</div>
+//           <div style={style}>Eight Slide</div>
+//         </Slide>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MultipleSlidesExample;
+
 import React from 'react';
 import "../App.css";
-import { Fade } from 'react-slideshow-image';
+import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
+
+const properties = {
+      duration: 1000,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      autoplay: true,
+      indicators: true,
+    };
 
 const slideImages = [
   {
     url: 'https://m.media-amazon.com/images/M/MV5BZjQxYTA3ODItNzgxMy00N2Y2LWJlZGMtMTRlM2JkZjI1ZDhhXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
-    caption: 'Slide 1'
+   
   },
   {
     url: 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
-    caption: 'Slide 2'
   },
   {
     url: 'https://m.media-amazon.com/images/M/MV5BMzkzMmU0YTYtOWM3My00YzBmLWI0YzctOGYyNTkwMWE5MTJkXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
-    caption: 'Slide 3'
   },
 ];
 
 const Slider = () => {
     return (
       <div className="slide-container">
-        <Fade>
+        <Slide {...properties}>
          {slideImages.map((slideImage, index)=> (
             <div className="each-slide" key={index}>
               <div className='slide' style={{'backgroundImage': `url(${slideImage.url})`}}>
@@ -29,7 +76,7 @@ const Slider = () => {
               </div>
             </div>
           ))} 
-        </Fade>
+        </Slide>
       </div>
     )
 }
