fcd140cdf2025acc74b386d9c81d943591797634
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-06-23 21:58:46 +0300

    last checks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-// import "./App.css";
 import axios from "axios";
-import { useState, useEffect } from "react";
 import { DataProvider } from "./Context/Context";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Pages from "./Router/Pages";
 import Data from "./Data/Data";
 
 function App() {
   return (
- 
     <DataProvider>
       <Pages />
-      <Data/>
+      <Data />
     </DataProvider>
   );
 }
